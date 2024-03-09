581889fd6db6c30b204be2603bd89a88cbdf715e
Brayen Luhat <bfenomenal@gmail.com>
2024-03-09 21:15:24 +0800

    add : rate limiter

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,20 @@ import apiRoutes from './routes/api'
 import session from 'express-session'
 import { PrismaSessionStore } from '@quixo3/prisma-session-store'
 import { prisma } from './utils/Db'
+import { rateLimit } from 'express-rate-limit'
 
 dotenv.config()
 
 const app: Express = express()
 const port: number | string = process.env.PORT ?? 3000
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minutes
+  limit: 150, // Limit each IP to 150 requests per `window` (here, per 1 minutes).
+  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers.
+})
 
+app.use(limiter)
 app.use(express.static('public'))
 app.use(express.json())
 app.use(
